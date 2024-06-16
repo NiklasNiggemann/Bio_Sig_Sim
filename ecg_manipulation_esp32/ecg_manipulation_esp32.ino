8bb0be2bc9ff87c3ea9cfdbf14bec7a5fc59fd92
Niklas <niklasniggemann@yahoo.de>
2024-06-16 16:57:30 +0200

    Initial Commit

--- ecg_manipulation_esp32/ecg_manipulation_esp32.ino
+++ ecg_manipulation_esp32/ecg_manipulation_esp32.ino
@@ -1,12 +1,19 @@
 #include <WiFi.h>
 #include <PubSubClient.h>
+#include <ESP32Servo.h>
+
+Servo my_servo_01;
 
 const char *ssid = "stud-hshl"; 
 const char *password = "stud-hshl2024"; 
 
-const char *mqtt_broker = "10.67.193.36";
-const char *topic = "ecg_manipulation_esp32_input";
+const char *mqtt_broker = "10.67.193.84";
 const int mqtt_port = 1883;
+
+int angle = 0;    
+int servo_01 = 13;
+int servo_02 = 12;
+bool standing = false;
 
 WiFiClient espClient;
 PubSubClient client(espClient);
@@ -14,6 +21,14 @@ PubSubClient client(espClient);
 void setup() 
 {
     Serial.begin(115200);
+
+    ESP32PWM::allocateTimer(0);
+	  ESP32PWM::allocateTimer(1);
+	  ESP32PWM::allocateTimer(2);
+	  ESP32PWM::allocateTimer(3);
+	  my_servo_01.setPeriodHertz(50);    
+	  my_servo_01.attach(servo_01, 1000, 2000); 
+    my_servo_01.write(0);
 
     WiFi.begin(ssid, password);
     while (WiFi.status() != WL_CONNECTED) 
@@ -40,27 +55,62 @@ void setup()
             delay(2000);
         }
     }
+    client.subscribe("position");
+}
 
-    client.publish(topic, "ESP32 (ECG manipulation) Connected");
-    client.subscribe(topic);
+void stand_up()
+{
+  for (angle = 180; angle >= 0; angle -= 1) 
+  { 
+		my_servo_01.write(angle);   
+		delay(5);             
+	}
+  standing = true;
+}
+
+void lay_down()
+{
+  for (angle = 0; angle <= 180; angle += 1) 
+  {  
+		my_servo_01.write(angle);   
+		delay(5);             
+	}
+  standing = false;
 }
 
 void callback(char *topic, byte *payload, unsigned int length) 
 {
-    Serial.print("Message arrived in topic: ");
-    Serial.println(topic);
-    Serial.print("Message: ");
-    for (int i = 0; i < length; i++) 
+  Serial.print("Message arrived in topic: ");
+  Serial.println(topic);
+
+  char message[length + 1];  
+  memcpy(message, payload, length);
+  message[length] = '\0';    
+
+  String strPayload = String(message);
+  Serial.print("Message: ");
+  Serial.println(strPayload);
+
+  if (strPayload == "standing")
+  {
+    if (!standing)
     {
-        Serial.print((char) payload[i]);
+      stand_up();
     }
-    Serial.println();
+  }
+  else if (strPayload == "laying")
+  {
+    if (standing)
+    {
+      lay_down(); 
+    }
+  }
 }
 
 void loop() 
 {
-    client.loop();
-    
+  client.loop();
+
 }
 
 
--- ecg_signal_esp32/ecg_signal_esp32.ino
+++ ecg_signal_esp32/ecg_signal_esp32.ino
@@ -5,7 +5,7 @@
 const char *ssid = "stud-hshl"; 
 const char *password = "stud-hshl2024"; 
 
-const char *mqtt_broker = "10.67.193.36";
+const char *mqtt_broker = "10.67.193.84";
 const char *topic = "ecg_signal_esp32_input";
 const int mqtt_port = 1883;
 
@@ -14,83 +14,93 @@ PubSubClient client(espClient);
 
 HardwareSerial SerialPico(2);
 
-int led_pin = 23;
+int led_pin = 0;
+int tilt_power_pin = 23; 
 int tilt_pin = 22; 
+int piezo_pin = 15; 
 
 void setup() 
 {
-    pinMode(led_pin, OUTPUT);
-    pinMode(tilt_pin, INPUT_PULLUP);
-    Serial.begin(115200);
-    SerialPico.begin(9600, SERIAL_8N1, 16, 17); 
+  pinMode(tilt_power_pin, OUTPUT);
+  digitalWrite(tilt_power_pin, HIGH);
+  pinMode(tilt_pin, INPUT_PULLUP);
+  pinMode(led_pin, OUTPUT);
+  Serial.begin(115200);
+  SerialPico.begin(9600, SERIAL_8N1, 16, 17); 
 
-    WiFi.begin(ssid, password);
-    while (WiFi.status() != WL_CONNECTED) 
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) 
+  {
+      delay(500);
+      Serial.println("Connecting to WiFi..");
+  }
+  Serial.println("Connected to Wi-Fi");
+
+  client.setServer(mqtt_broker, mqtt_port);
+  client.setCallback(callback);
+  while (!client.connected()) 
+  {
+    String client_id = String(WiFi.macAddress());
+    Serial.println("Connecting to Broker..");
+    if (client.connect(client_id.c_str())) 
     {
-        delay(500);
-        Serial.println("Connecting to WiFi..");
+        Serial.println("Connected to Broker");
+    } 
+    else 
+    {
+        Serial.print("Failed with state ");
+        Serial.println(client.state());
+        delay(2000);
     }
-    Serial.println("Connected to Wi-Fi");
+  }
 
-    client.setServer(mqtt_broker, mqtt_port);
-    client.setCallback(callback);
-    while (!client.connected()) {
-        String client_id = String(WiFi.macAddress());
-        Serial.println("Connecting to Broker..");
-        if (client.connect(client_id.c_str())) 
-        {
-            Serial.println("Connected to Broker");
-        } 
-        else 
-        {
-            Serial.print("failed with state ");
-            Serial.print(client.state());
-            delay(2000);
-        }
-    }
+  client.publish(topic, "ESP32 (ECG signal) Connected");
+  client.subscribe(topic);
+}
 
-    client.publish(topic, "ESP32 (ECG signal) Connected");
-    client.subscribe(topic);
+void measure_position()
+{
+  if (digitalRead(tilt_pin) == 0)
+  {
+    //client.publish("measured_position", "laying");
+  }
+  else if (digitalRead(tilt_pin) == 1)
+  {
+    //client.publish("measured_position", "standing");
+  }
 }
 
 void callback(char *topic, byte *payload, unsigned int length) 
 {
-    /*Serial.print("Message arrived in topic: ");
-    Serial.println(topic);
-    Serial.print("Message: ");
-    for (int i = 0; i < length; i++) 
-    {
-        Serial.print((char) payload[i]);
-    }
-    Serial.println();*/
+  // Process incoming messages if needed
 }
 
 void loop() 
 {
   client.loop();
-  Serial.println(digitalRead(tilt_pin));
   if (SerialPico.available())
   {
-    String ecg_sample_string = String(SerialPico.read());
-    float ecg_sample_value = ecg_sample_string.toFloat() - 48;
-    ecg_sample_string = String(ecg_sample_value);
-    if (ecg_sample_value != -38)
+    String ecg_sample_string = SerialPico.readStringUntil('\n'); 
+    float ecg_sample_value = ecg_sample_string.toFloat();
+    Serial.println("ECG Sample Value: " + String(ecg_sample_value));
+    if (ecg_sample_value != 0.0) 
     {
-      client.publish(topic, ecg_sample_string.c_str());
-      if (ecg_sample_value == 4)
+      client.publish(topic, String(ecg_sample_value).c_str());
+      if (ecg_sample_value == 4.0)  
       {
         client.publish("ecg_signal_peak_esp32_input", "peak");
-        peak_led();
+        peak_alarm();
       }
     }
   }
-  delay(10); 
+  measure_position();
 }
 
-void peak_led()
+void peak_alarm()
 {
-  digitalWrite(led_pin, HIGH);
-  delay(50);
-  digitalWrite(led_pin, LOW);
+    tone(piezo_pin, 500);
+    digitalWrite(led_pin, HIGH);
+    delay(20);
+    digitalWrite(led_pin, LOW);
+    noTone(piezo_pin);
 }
-
